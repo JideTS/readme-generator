5e5ae7c0a05bfc1978e22ef383d96153b6d7937c
Shaduruk <jide@tidjani-serpos.com>
2023-09-09 23:13:10 -0400

    add application code to have it perform as directed by user stories

---
+++ index.js
@@ -0,0 +1,97 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+inquirer
+  .prompt([
+    
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the title of the application?',
+    },
+
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Please describe the installation process of the application.',
+      },
+
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'Please describe how the installation should be used.',
+      },
+
+      {
+        type: 'input',
+        name: 'contributions',
+        message: 'Please describe how other developers can contribute to the application.',
+      },
+
+      {
+        type: 'input',
+        name: 'tests',
+        message: 'Please explain how to test the application and provide examples on how to run them.',
+      },
+
+      {
+        type: 'list',
+        message: 'What type of licence would you like to include with your application?',
+        name: 'licence',
+        choices: ['None', 'MIT', 'Apache 2.0', 'GNU General Public Licence v3.0'],
+      },
+
+
+      {
+        type: 'input',
+        name: 'gitHubUserName',
+        message: 'Please type the github username you want to use for this application.',
+      },
+
+      {
+        type: 'input',
+        name: 'email',
+        message: 'Please type the email address other developers can use to contact you about this application.',
+      },
+      
+  ])
+  .then((data) => {
+
+    let licenceMarkdown = "";
+
+    function returnlicenceMarkdown() {
+     
+     switch (data.licence) {
+        case 'MIT':
+          licenceMarkdown = "![Static Badge](https://img.shields.io/badge/MIT_Licence-blue)";
+          break;
+        case 'Apache 2.0':
+            licenceMarkdown = "![Static Badge](https://img.shields.io/badge/Apache_2.0_Licence-blue)";
+          break;
+        case 'GNU General Public Licence v3.0':
+            licenceMarkdown = "![Static Badge](https://img.shields.io/badge/GNU_General_Public_Licence_v3.0-blue)";
+          break;
+        case 'None':
+            licenceMarkdown = "![Static Badge](https://img.shields.io/badge/No_Licence-blue)";
+          break;
+        default:
+            licenceMarkdown = "![Static Badge](https://img.shields.io/badge/No_Licence-blue)";
+      }
+     
+      return licenceMarkdown;
+
+    }
+
+    returnlicenceMarkdown();
+
+    function generateMarkdown() {
+        return `${licenceMarkdown}\n\n #${data.title}\n\n##Installation\n ${data.installation}\n\n##Usage\n ${data.usage}\n\n##Contributions\n ${data.contributions}\n\n##Tests\n ${data.tests}\n\n##Questions\n Please address your questions to ${data.email} or visit my GitHub profile at https://github.com/${data.gitHubUserName} \n\n##Licence\n ${data.licence} 
+      `;
+      }
+    fs.writeFile("README.md", generateMarkdown(), (err) =>
+      err ? console.log(err) : console.log('Your README file was successfully generated.')
+    );
+  });
+
+
+
